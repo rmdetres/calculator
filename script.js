7d448a61a8810c2c040066580cf0f2d88502264b
Ryan <rmdetres@gmail.com>
2024-05-22 20:53:35 -0400

    adding the dynamic display

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ let numbersArray =[];
 let numbersArrayBeforeAction = [];
 let captureOperator = '';
 let newCaptureOperator= [];
+let displayTotal = [];
+
+
 
 //add event listers
 const zeroButton = document.getElementById('0');
@@ -10,6 +13,8 @@ zeroButton.addEventListener('click', function() {
 console.log("I clicked zero")
 numbersArray.push(0);
 console.log(numbersArray);
+displayTotal.push(0);
+document.getElementById('result').innerHTML = (String(displayTotal.join('')));
 
 });
 
@@ -17,56 +22,83 @@ const oneButton = document.getElementById('1');
 oneButton.addEventListener('click', function() {
 numbersArray.push(1);
 console.log(numbersArray);   
-console.log("I clicked one")});
+console.log("I clicked one")
+displayTotal.push(1);
+document.getElementById('result').innerHTML = (String(displayTotal.join('')));
+});
 
 
 const twoButton = document.getElementById('2');
 twoButton.addEventListener('click', function() {
 numbersArray.push(2);
 console.log(numbersArray);   
-console.log("I clicked two")});
+console.log("I clicked two")
+displayTotal.push(2);
+document.getElementById('result').innerHTML = (String(displayTotal.join('')));
+});
 
 const threeButton = document.getElementById('3');
 threeButton.addEventListener('click', function() {
 numbersArray.push(3);
 console.log(numbersArray);
-console.log("I clicked three")});
+console.log("I clicked three");
+displayTotal.push(3);
+document.getElementById('result').innerHTML = (String(displayTotal.join('')));
+});
 
 const fourButton = document.getElementById('4');
 fourButton.addEventListener('click', function() {
 numbersArray.push(4);
 console.log(numbersArray);
-console.log("I clicked four")});
+console.log("I clicked four");
+displayTotal.push(4);
+document.getElementById('result').innerHTML = (String(displayTotal.join('')));
+});
 
 const fiveButton = document.getElementById('5');
 fiveButton.addEventListener('click', function() {
 numbersArray.push(5);
 console.log(numbersArray);
-console.log("I clicked five")});
+console.log("I clicked five")
+displayTotal.push(5);
+document.getElementById('result').innerHTML = (String(displayTotal.join('')));});
 
 const sixButton = document.getElementById('6');
 sixButton.addEventListener('click', function() {
 numbersArray.push(6);
 console.log(numbersArray);
-console.log("I clicked six")});
+console.log("I clicked six");
+displayTotal.push(6);
+document.getElementById('result').innerHTML = (String(displayTotal.join('')));
+});
 
 const sevenButton = document.getElementById('7');
 sevenButton.addEventListener('click', function() {
 numbersArray.push(7);
 console.log(numbersArray);
-console.log("I clicked 7")});
+console.log("I clicked 7");
+displayTotal.push(7);
+document.getElementById('result').innerHTML = (String(displayTotal.join('')));
+
+});
 
 const eightButton = document.getElementById('8');
 eightButton.addEventListener('click', function() {
 numbersArray.push(8);
 console.log(numbersArray);
-console.log("I clicked 8")});
+console.log("I clicked 8");
+displayTotal.push(8);
+document.getElementById('result').innerHTML = (String(displayTotal.join('')));
+});
 
 const nineButton = document.getElementById('9');
 nineButton.addEventListener('click', function() {
 numbersArray.push(9);
 console.log(numbersArray);
-console.log("I clicked 9")});
+console.log("I clicked 9");
+displayTotal.push(9);
+document.getElementById('result').innerHTML = (String(displayTotal.join('')));
+});
 
 
 
@@ -80,7 +112,9 @@ divideButton.addEventListener('click', function() {
     numbersArrayBeforeAction.push(lastNumber);
     numbersArray.length = 0;
     console.log(numbersArrayBeforeAction);
-    console.log("I clicked /")});
+    console.log("I clicked /")
+    displayTotal.push('/');
+    document.getElementById('result').innerHTML = (String(displayTotal.join('')));});
 
 const multiplyButton = document.getElementById('*');
 multiplyButton.addEventListener('click', function() {
@@ -89,8 +123,10 @@ multiplyButton.addEventListener('click', function() {
     numbersArrayBeforeAction.push(lastNumber);
     numbersArray.length = 0;
     console.log(numbersArrayBeforeAction);
-
-    console.log("I clicked *")});
+    console.log("I clicked *")
+    displayTotal.push('*');
+    document.getElementById('result').innerHTML = (String(displayTotal.join('')));
+});
 
 const minusButton = document.getElementById('-');
 minusButton.addEventListener('click', function() {
@@ -99,9 +135,9 @@ minusButton.addEventListener('click', function() {
       numbersArrayBeforeAction.push(lastNumber);
       numbersArray.length = 0;
       console.log(numbersArrayBeforeAction);
-      
-  //  numbersArray.length = 0;
-    console.log("I clicked -"); });
+      console.log("I clicked -");
+      displayTotal.push('-');
+      document.getElementById('result').innerHTML = (String(displayTotal.join(''))); });
 
 
 ///working on this one
@@ -112,21 +148,31 @@ plusButton.addEventListener('click', function() {
     numbersArrayBeforeAction.push(lastNumber);
     numbersArray.length = 0;
     console.log(numbersArrayBeforeAction);
-    console.log("I clicked +")});
+    console.log("I clicked +")
+    displayTotal.push('+');
+    document.getElementById('result').innerHTML = (String(displayTotal.join('')));
+});
 
   
 
 const pointButton = document.getElementById('.');
 pointButton.addEventListener('click', function() {
     numbersArray.push('.');
-    console.log("I clicked .")});
+    console.log("I clicked .");
+    displayTotal.push('.');
+    document.getElementById('result').innerHTML = (String(displayTotal.join('')));
+
+});
 
 const clearButton = document.getElementById('c');
 clearButton.addEventListener('click', function() {
     numbersArray.length = 0;
     numbersArrayBeforeAction.length = 0;
     document.getElementById('result').innerHTML = '';
-    console.log("I clicked C")});
+    console.log("I clicked C");
+    displayTotal.length = 0;
+
+});
 
 
 const equalButton = document.getElementById('=');
